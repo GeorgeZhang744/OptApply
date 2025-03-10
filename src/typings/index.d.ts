b1d216512c25338e8d3dfb2cc5be44fdfd6d12a8
GeorgeZhang744 <georgezhang744@gmail.com>
2025-03-10 11:25:40 -0400

    feat: init project setup
    Init Vite-React project template and remove the default looking
    Integrate TailwindCSS
    Create necessary folders including:
      - pages: storing the Page view React component
      - components: storing modular, reusable React components needed in building page views
      - data: storing mockdata files
      - contexts: storing React context
      - typings: storing files for global typings
    Create basic React components including:
      - CalenderPage.tsx
      - LandingPage.tsx
      - MainPage.tsx
      - SignInPage.tsx
      - SignUpPage.tsx
      - JobCard.tsx
      - JobList.tsx
      - NavBar.tsx

---
+++ src/typings/index.d.ts
@@ -0,0 +1,9 @@
+namespace models {
+  // Example usage of this type directly in other files: models.example.ExampleType
+  namespace example {
+    interface ExampleType {
+      exampleText: string;
+      exampleVal: number;
+    }
+  }
+}
