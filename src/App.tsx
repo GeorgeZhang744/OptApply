d11596173bcf65037a1c90fe1f7e5813025af83c
AdamKaluzny <adam.kaluzny222@gmail.com>
2025-03-11 17:34:07 -0400

    feat: add navbar
    -add inital navbar seen on landing page
    -add logo to asset folder to use for navbar
    
    Works towards resolving #18

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
+import NavBar from "./components/NavBar/NavBar";
 
 function App() {
   return (
     <Router>
       <div className="text-center">
+        <NavBar/>
         <Routes>
           <Route path="/" element={<LandingPage />} />
         </Routes>
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,7 +1,25 @@
+import { Link } from "react-router-dom";
+import logo from "../../assets/logo.png";
+
 const NavBar = () => {
   return (
-    <div>NavBar</div>
-  )
-}
+    <nav className="navbar bg-gray-200 px-6 py-3 fixed top-0 left-0 w-full shadow-md flex items-center">
 
-export default NavBar
+      <div className="flex items-center space-x-2">
+        <img src={logo} alt="OptApply Logo" className="w-10 h-10" />
+        <span className="text-gray-800 font-semibold">OptApply</span>
+      </div>
+
+      <div className="ml-auto flex space-x-3">
+        <Link to="/login">
+          <button className="btn btn-sm btn-soft bg-red-200 text-red-800">Login</button>
+        </Link>
+        <Link to="/signup">
+          <button className="btn btn-sm btn-soft bg-red-200 text-red-800">Sign Up</button>
+        </Link>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
