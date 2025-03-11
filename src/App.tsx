0a2f317dd530d905ca00a9b318b4fbea1645b2e1
AdamKaluzny <adam.kaluzny222@gmail.com>
2025-03-11 15:48:17 -0400

    chore: add react-router to dependencies and set landing page as intial route
     Works toward resolving #20

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
 
 function App() {
   return (
-    <div className="text-center">
-      OptApply
-    </div>
+    <Router>
+      <div className="text-center">
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
