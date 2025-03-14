8dcdab55b1ddcd0cf343ed3eb48cfc13d39dd1ac
Nick Wierzbowski <nwierzbow@gmail.com>
2025-03-13 22:34:53 -0400

    Implemented daisyUI themes

--- src/pages/SignInPage.tsx
+++ src/pages/SignInPage.tsx
@@ -16,24 +16,24 @@ const SignInPage = () => {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 px-4">
+    <div className="flex flex-col items-center justify-center min-h-screen px-4">
       
       <div className="flex flex-col items-center mb-6">
         <img src={logo} alt="Logo" className="w-30 h-30" />
       </div>
 
       
-      <div className="bg-white p-8 shadow-md rounded-lg w-full max-w-md">
-        <h2 className="text-2xl font-semibold text-center mb-4 text-gray-700">Sign In</h2>
+      <div className="bg-neutral p-8 shadow-md rounded-lg w-full max-w-md">
+        <h2 className="text-2xl font-semibold text-center mb-4">Sign In</h2>
 
         
         <form onSubmit={handleLogin} className="flex flex-col space-y-4">
           
           <div>
-            <label className="flex justify-start text-sm font-medium text-gray-700">Email</label>
+            <label className="flex justify-start text-sm font-medium">Email</label>
             <input
               type="email"
-              className="input input-bordered w-full mt-1 bg-gray-400 placeholder-gray-200"
+              className="input input-bordered w-full mt-1"
               value={email}
               onChange={email => setEmail(email.target.value)}
               placeholder="Enter Email"
@@ -43,10 +43,10 @@ const SignInPage = () => {
 
           
           <div>
-            <label className="flex justify-start text-sm font-medium text-gray-700">Password</label>
+            <label className="flex justify-start text-sm font-medium">Password</label>
             <input
               type="password"
-              className="input input-bordered w-full mt-1 bg-gray-400 placeholder-gray-200"
+              className="input input-bordered w-full mt-1"
               value={password}
               onChange={password => setPassword(password.target.value)}
               placeholder="Enter Password"
@@ -55,13 +55,13 @@ const SignInPage = () => {
           </div>
 
           
-          <button type="submit" className="btn btn-soft bg-red-200 text-red-800">
+          <button type="submit" className="btn btn-success btn-soft ">
             Sign In
           </button>
         </form>
 
         <div className="flex justify-end mt-3 text-sm">
-          <Link to="/signup" className="text-red-800 font-semibold">
+          <Link to="/signup" className="text-info font-semibold">
             Sign up
           </Link>
         </div>
--- src/pages/SignUpPage.tsx
+++ src/pages/SignUpPage.tsx
@@ -25,25 +25,25 @@ const SignUpPage = () => {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 px-4">
+    <div className="flex flex-col items-center justify-center min-h-screen px-4">
       
       <div className="flex flex-col items-center mb-6">
         <img src={logo} alt="Logo" className="w-30 h-30" />
       </div>
 
       
-      <div className="bg-white p-8 shadow-md rounded-lg w-full max-w-md">
-        <h2 className="text-2xl font-semibold text-center mb-4 text-gray-700">Sign Up</h2>
+      <div className="bg-neutral p-8 shadow-md rounded-lg w-full max-w-md">
+        <h2 className="text-2xl font-semibold text-center mb-4">Sign Up</h2>
         {passwordError && (
-              <p className="text-red-500 text-sm mt-1">Passwords do not match.</p>
+              <p className="text-error text-sm mt-1">Passwords do not match.</p>
           )}
         <form onSubmit={handleSignup} className="flex flex-col space-y-4">
           
           <div>
-            <label className="flex justify-start text-sm font-medium text-gray-700">Email</label>
+            <label className="flex justify-start text-sm font-medium">Email</label>
             <input
               type="email"
-              className="input input-bordered w-full mt-1 bg-gray-400 placeholder-gray-200"
+              className="input input-bordered w-full mt-1"
               value={email}
               onChange={email => setEmail(email.target.value)}
               placeholder="Enter Email"
@@ -53,10 +53,10 @@ const SignUpPage = () => {
 
           
           <div>
-            <label className="flex justify-start text-sm font-medium text-gray-700">Password</label>
+            <label className="flex justify-start text-sm font-medium">Password</label>
             <input
               type="password"
-              className="input input-bordered w-full mt-1 bg-gray-400 placeholder-gray-200"
+              className="input input-bordered w-full mt-1"
               value={password}
               onChange={password => setPassword(password.target.value)}
               placeholder="Set Password"
@@ -65,10 +65,10 @@ const SignUpPage = () => {
           </div>
 
           <div>
-            <label className="flex justify-start text-sm font-medium text-gray-700">Re-Enter Password</label>
+            <label className="flex justify-start text-sm font-medium">Re-Enter Password</label>
             <input
               type="password"
-              className="input input-bordered w-full mt-1 bg-gray-400 placeholder-gray-200"
+              className="input input-bordered w-full mt-1"
               value={password2}
               onChange={password2 => setPassword2(password2.target.value)}
               placeholder="Re-Enter Password"
@@ -77,13 +77,13 @@ const SignUpPage = () => {
           </div>
 
           
-          <button type="submit" className="btn btn-soft bg-red-200 text-red-800" >
+          <button type="submit" className="btn btn-success btn-soft" >
             Sign Up
           </button>
         </form>
 
         <div className="flex justify-end mt-3 text-sm">
-          <Link to="/login" className="text-red-800 font-semibold">
+          <Link to="/login" className="text-info font-semibold">
             Login
           </Link>
         </div>
