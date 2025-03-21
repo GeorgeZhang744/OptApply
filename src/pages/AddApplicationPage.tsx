9ffd1ac1002e6e2380c66e9ccccfd3f834b8b19e
GeorgeZhang744 <georgezhang744@gmail.com>
2025-03-21 01:18:39 -0400

    refactor: update Add Application Page to use Application Form component instead

--- src/pages/AddApplicationPage.tsx
+++ src/pages/AddApplicationPage.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import ApplicationForm from "../components/ApplicationForm/ApplicationForm";
 
 const AddApplicationPage = () => {
   const [url, setUrl] = useState("");
@@ -22,7 +23,19 @@ const AddApplicationPage = () => {
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target;
-    setFormData((prevData) => ({ ...prevData, [name]: value }));
+    if (name === "salaryMin" || name === "salaryMax") {
+      // Handle salary input update
+      setFormData((prevData) => ({
+        ...prevData,
+        salary: {
+          ...prevData.salary,
+          [name === "salaryMin" ? "min" : "max"]: parseInt(value, 10) || 0,
+        },
+      }));
+    } else {
+      // Handle other input update
+      setFormData((prevData) => ({ ...prevData, [name]: value }));
+    }
   };
 
   const handleSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
@@ -73,185 +86,21 @@ const AddApplicationPage = () => {
         </div>
       </div>
 
-      {/* Application Details Form */}
-      <div>
-        <h2 className="text-xl font-semibold mb-2">Application Details</h2>
-        <form className="space-y-4">
-          {/* Company and Position */}
-          <div className="flex flex-col md:flex-row md:space-x-2">
-            <div className="flex-1">
-              <label className="block text-sm font-medium">
-                Company Name <span className="text-red-500">*</span>
-              </label>
-              <input
-                type="text"
-                name="company"
-                className="input input-bordered w-full"
-                value={formData.company}
-                onChange={handleInputChange}
-                required
-              />
-            </div>
-            <div className="flex-1">
-              <label className="block text-sm font-medium">
-                Position <span className="text-red-500">*</span>
-              </label>
-              <input
-                type="text"
-                name="position"
-                className="input input-bordered w-full"
-                value={formData.position}
-                onChange={handleInputChange}
-                required
-              />
-            </div>
-          </div>
+      <ApplicationForm
+        formData={formData}
+        handleInputChange={handleInputChange}
+        handleSelectChange={handleSelectChange}
+        handleTextareaChange={handleTextareaChange}
+        isEditable={true}
+      />
 
-          {/* Application URL and Deadline */}
-          <div className="flex flex-col md:flex-row md:space-x-2">
-            <div className="flex-1">
-              <label className="block text-sm font-medium">
-                Application URL <span className="text-red-500">*</span>
-              </label>
-              <input
-                type="text"
-                name="applicationUrl"
-                className="input input-bordered w-full"
-                value={formData.applicationUrl}
-                onChange={handleInputChange}
-                required
-              />
-            </div>
-            <div className="flex-1">
-              <label className="block text-sm font-medium">
-                Deadline <span className="text-red-500">*</span>
-              </label>
-              <input
-                type="date"
-                name="deadline"
-                className="input input-bordered w-full"
-                value={formData.deadline}
-                onChange={handleInputChange}
-                required
-              />
-            </div>
-          </div>
-
-          {/* Work Location and Status */}
-          <div className="flex flex-col md:flex-row md:space-x-2">
-            <div className="flex-1">
-              <label className="block text-sm font-medium">
-                Work Location <span className="text-red-500">*</span>
-              </label>
-              <select
-                name="workLocation"
-                className="input input-bordered w-full"
-                value={formData.workLocation}
-                onChange={handleSelectChange}
-                required
-              >
-                <option value="Remote">Remote</option>
-                <option value="On-site">On-site</option>
-                <option value="Hybrid">Hybrid</option>
-              </select>
-            </div>
-            <div className="flex-1">
-              <label className="block text-sm font-medium">
-                Status <span className="text-red-500">*</span>
-              </label>
-              <select
-                name="status"
-                className="input input-bordered w-full"
-                value={formData.status}
-                onChange={handleSelectChange}
-                required
-              >
-                <option value="Applied">Applied</option>
-                <option value="Interviewing">Interviewing</option>
-                <option value="Offered">Offered</option>
-                <option value="Rejected">Rejected</option>
-              </select>
-            </div>
-          </div>
-
-          {/* Salary Range */}
-          <div>
-            <label className="block text-sm font-medium">
-              Salary Range <span className="text-red-500">*</span>
-            </label>
-            <div className="flex flex-col md:flex-row md:space-x-2">
-              <div className="flex-1">
-                <label className="block text-xs font-medium">Min</label>
-                <input
-                  type="number"
-                  name="salaryMin"
-                  placeholder="Min"
-                  className="input input-bordered w-full"
-                  value={formData.salary.min}
-                  onChange={(e) => setFormData({ ...formData, salary: { ...formData.salary, min: parseInt(e.target.value) } })}
-                  required
-                />
-              </div>
-              <div className="flex-1">
-                <label className="block text-xs font-medium">Max</label>
-                <input
-                  type="number"
-                  name="salaryMax"
-                  placeholder="Max"
-                  className="input input-bordered w-full"
-                  value={formData.salary.max}
-                  onChange={(e) => setFormData({ ...formData, salary: { ...formData.salary, max: parseInt(e.target.value) } })}
-                  required
-                />
-              </div>
-            </div>
-          </div>
-
-          {/* Skills Required */}
-          <div>
-            <label className="block text-sm font-medium">Skills Required</label>
-            <input
-              type="text"
-              name="skillsRequired"
-              className="input input-bordered w-full"
-              value={formData.skillsRequired}
-              onChange={handleInputChange}
-            />
-          </div>
-
-          {/* Job Description */}
-          <div>
-            <label className="block text-sm font-medium">Job Description</label>
-            <textarea
-              name="jobDescription"
-              className="textarea input-bordered w-full"
-              value={formData.jobDescription}
-              onChange={handleTextareaChange}
-              rows={5}
-            />
-          </div>
-
-          {/* Note */}
-          <div>
-            <label className="block text-sm font-medium">Note</label>
-            <textarea
-              name="note"
-              className="textarea input-bordered w-full"
-              value={formData.note}
-              onChange={handleTextareaChange}
-            />
-          </div>
-
-          {/* Form Actions */}
-          <div className="flex justify-end space-x-2 mt-4">
-            <Link to="/home" className="btn btn-secondary">
-              Cancel
-            </Link>
-            <button type="submit" className="btn btn-primary">
-              Submit
-            </button>
-          </div>
-        </form>
+      <div className="flex justify-end space-x-2 mt-4">
+        <Link to="/home" className="btn btn-secondary">
+          Cancel
+        </Link>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
       </div>
     </div>
   );
