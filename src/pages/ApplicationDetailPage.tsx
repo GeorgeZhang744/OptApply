3c10713900941001852671c35bb4a624213ae4fb
GeorgeZhang744 <georgezhang744@gmail.com>
2025-03-21 01:24:02 -0400

    feat: add edit button in Application Detail Page that navigates to Edit Application Page

--- src/pages/ApplicationDetailPage.tsx
+++ src/pages/ApplicationDetailPage.tsx
@@ -1,26 +1,37 @@
-import {useEffect, useState} from "react";
-import { useParams } from "react-router";
-import {mockApplications} from "../data/mockdata";
+import { useEffect, useState } from "react";
+import { useParams, Link } from "react-router";
+import { mockApplications } from "../data/mockdata";
 import JobCard from "../components/JobCard/JobCard";
 
 const ApplicationDetailPage = () => {
   const { applicationId } = useParams();
   const [application, setApplication] = useState<models.application.IApplication | null>(null);
-
-  useEffect(()=>{
-    const fetchApplication = async()=>{
+  
+  useEffect(() => {
+    const fetchApplication = async () => {
       const foundApplication = mockApplications.find((app) => app.id === applicationId) || null;
-      
+
       //simulate API call delay
-      setTimeout(()=> setApplication(foundApplication),500);
+      setTimeout(() => setApplication(foundApplication), 500);
     };
 
     fetchApplication();
-  }, [applicationId]); 
+  }, [applicationId]);
 
   return (
     <div className="container w-full mx-auto mt-24">
-      {application ? <JobCard application={application}/> : <p>Loading...</p>}
+      {application ? (
+        <>
+          <JobCard application={application} />
+          <div className="flex justify-end space-x-2 mt-4">
+            <Link to={`/edit/${applicationId}`} className="btn btn-primary">
+              Edit
+            </Link>
+          </div>
+        </>
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
   );
 };
