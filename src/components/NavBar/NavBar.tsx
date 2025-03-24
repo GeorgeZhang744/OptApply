d8ee2dbe71dda18e92cec71e2d2c76cae2c6aee1
AdamKaluzny <adam.kaluzny222@gmail.com>
2025-03-23 21:36:43 -0400

    fix: modify login logic
    change logic to determine if user is logged in
    prior improper logic led to incorrect navbar appearance at certain areas of site
    
    solves #34 an works toward resolving #18

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,20 +1,32 @@
 import { Link, useLocation } from "react-router-dom";
 import logo from "../../assets/logo.png";
+import { FaCalendar } from "react-icons/fa";
+
 
 const NavBar = () => {
   const curLocation = useLocation();
-  const loggedIn = curLocation.pathname == "/home";
+  const loggedIn = !(curLocation.pathname == "/" || curLocation.pathname == "/login" || curLocation.pathname == "/signup");
   return (
     <nav className="navbar bg-primary px-6 py-3 fixed top-0 left-0 w-full shadow-md flex items-center z-50">
-      <Link to="/">
-        <div className="flex items-center space-x-2">
-          <img src={logo} alt="OptApply Logo" className="w-10 h-10" />
-          <span className="text-primary-content font-semibold">OptApply</span>
-        </div>
-      </Link>
+      {loggedIn ? (
+        <Link to="/home">
+          <div className="flex items-center space-x-2">
+            <img src={logo} alt="OptApply Logo" className="w-10 h-10" />
+            <span className="text-primary font-semibold">OptApply</span>
+          </div>
+        </Link>
+      ) : (
+        <Link to="/">
+          <div className="flex items-center space-x-2">
+            <img src={logo} alt="OptApply Logo" className="w-10 h-10" />
+            <span className="text-primary font-semibold">OptApply</span>
+          </div>
+        </Link>
+      )}
       <div className="ml-auto flex space-x-3">
         {loggedIn ? (
-          <div>
+          <div className="ml-auto flex space-x-3 items-center">
+            <FaCalendar size={30}/>
             <Link to="/">
               <button className="btn btn-error btn-sm btn-soft">Sign Out</button>
             </Link>
