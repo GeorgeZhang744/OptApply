e3f1171014f014ce83fcd7daaadadd37db0c0d19
GeorgeZhang744 <georgezhang744@gmail.com>
2025-03-28 18:17:06 -0400

    feat: optimize UI rendering performance
    - Use `useCallback` to prevent function re-creations in AddApplicationPage, EditApplicationPage, and MainPage
    - Use `useMemo` to prevent potential  expensive calculation (once when real backend and database are involved) in  AddApplicationPage, EditApplicationPage, ApplicationDetailPage, and MainPage
    - Convert the  image `mockMainPageView.png` into `webp` format to reduce size

--- src/pages/ApplicationDetailPage.tsx
+++ src/pages/ApplicationDetailPage.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useMemo } from "react";
 import { useParams, Link } from "react-router";
 import { mockApplications } from "../data/mockdata";
 import JobCard from "../components/JobCard/JobCard";
@@ -6,17 +6,20 @@ import JobCard from "../components/JobCard/JobCard";
 const ApplicationDetailPage = () => {
   const { applicationId } = useParams();
   const [application, setApplication] = useState<models.application.IApplication | null>(null);
-  
+
+  const foundApplication = useMemo(
+    () => mockApplications.find((application) => application.id === applicationId) || null,
+    [applicationId]
+  );
+
   useEffect(() => {
     const fetchApplication = async () => {
-      const foundApplication = mockApplications.find((app) => app.id === applicationId) || null;
-
       //simulate API call delay
       setTimeout(() => setApplication(foundApplication), 500);
     };
 
     fetchApplication();
-  }, [applicationId]);
+  }, [foundApplication]);
 
   return (
     <div className="container w-full mx-auto mt-24">
