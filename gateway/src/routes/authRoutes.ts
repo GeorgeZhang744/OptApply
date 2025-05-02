80363552853acdbb880d46bd22295144451c2c4e
GeorgeZhang744 <georgezhang744@gmail.com>
2025-05-02 15:51:32 -0400

    feat: update proxy routers to use pathrewrite to rewrite the redirected path instead of directly modifying the path of the request

--- gateway/src/routes/authRoutes.ts
+++ gateway/src/routes/authRoutes.ts
@@ -6,20 +6,27 @@ const router = Router();
 const PROXY_TARGET = "http://localhost:3001";
 
 router.post("/signup", (req, res, next) => {
-  req.url = "/auth/signup";
+  // req.url = "/auth/signup";
   createProxyMiddleware({
     target: PROXY_TARGET,
     changeOrigin: true,
+    pathRewrite: {
+      "^/signup": "/auth/signup",
+    },
+    logger: console,
   })(req, res, next);
 });
 
 router.post("/signin", (req, res, next) => {
-  req.url = "/auth/signin";
+  // req.url = "/auth/signin";
   createProxyMiddleware({
     target: PROXY_TARGET,
     changeOrigin: true,
+    pathRewrite: {
+      "^/signin": "/auth/signin",
+    },
+    logger: console,
   })(req, res, next);
 });
-
 
 export default router;
